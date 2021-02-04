77a8c1f2cf4733758f09592c88009929ccc00b8c
Omar <omarelzhery236@gmail.com>
2021-02-04 14:46:20 +0200

    init_1

---
+++ js/funcs.js
@@ -0,0 +1,41 @@
+// Add Class To item And Remove From sibilings
+const active_Class = (parent_query_selector, child_node_name) => {
+  document
+    .querySelector(parent_query_selector)
+    .addEventListener("click", (e) => {
+      let target = e.target;
+      if (target.nodeName == child_node_name) {
+        Array.from(target.parentElement.children).forEach((li) => {
+          li.classList.remove("active");
+        });
+        target.classList.add("active");
+      }
+    });
+};
+
+// Scroll To Top
+const scroll_Top = (button_selector) => {
+  let scroll_top_button = document.querySelector(button_selector);
+  scroll_top_button.addEventListener("click", scrollTop);
+  function scrollTop() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+};
+
+// Switch Dom (right To left) (left To right)
+const lang_switch = (button_selector) => {
+  let language_button = document.getElementById(button_selector);
+  language_button.addEventListener("click", function () {
+    if (language_button.textContent == "Arabic") {
+      document.documentElement.setAttribute("dir", "rtl");
+      language_button.textContent = "English";
+    } else {
+      document.documentElement.setAttribute("dir", "ltr");
+      language_button.textContent = "Arabic";
+    }
+  });
+};
