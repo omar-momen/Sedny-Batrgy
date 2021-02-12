505b59f9c7546203eee151041885d2f8427b1c8f
Omar <omarelzhery236@gmail.com>
2021-02-12 17:21:09 +0200

    faq_section

--- js/pages/services.js
+++ js/pages/services.js
@@ -1,3 +1,10 @@
+// Filters
 $(".services_filter .filter_section .upper").click(function () {
   $(this).parent().find("form").slideToggle();
+});
+
+// Faq
+$(".services_filter .faq .accordions .accordion h2").click(function () {
+  $(this).next(".accordion_content").slideToggle();
+  $(this).find("span:last-child").toggleClass("active");
 });
