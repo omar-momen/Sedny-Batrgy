b9d7855ef28827ef0440fab99590687865efddc5
Omar <omarelzhery236@gmail.com>
2021-02-16 20:33:41 +0200

    navbar_round_tow

--- js/main.js
+++ js/main.js
@@ -5,16 +5,12 @@ $(window).on("load", function () {
 /********** End loading **********/
 
 /********** Start Switch Dom (right To left) (left To right) **********/
-let language_button = document.querySelector(
-  "nav.nav .upper_nav .switch_lang .glope"
-);
+let language_button = document.querySelector("nav.navbar .glope");
 let siwtch_lang = () => {
   if (document.documentElement.getAttribute("dir") == "ltr") {
     document.documentElement.setAttribute("dir", "rtl");
-    language_button.nextElementSibling.textContent = "En";
   } else if (document.documentElement.getAttribute("dir") == "rtl") {
     document.documentElement.setAttribute("dir", "ltr");
-    language_button.nextElementSibling.textContent = "Ar";
   }
 };
 language_button.addEventListener("click", siwtch_lang);
@@ -22,105 +18,91 @@ language_button.addEventListener("click", siwtch_lang);
 
 // Adjust main Header_height
 const main_header = document.querySelector("header.header");
-const nav = document.querySelector("nav.nav");
+const nav = document.querySelector("nav.navbar");
 const nav_height_value = nav.clientHeight;
 const window_height = window.innerHeight;
 if (main_header) {
   main_header.style.height = window_height - nav_height_value + "px";
 }
-// Main Nav Search input
-$("nav .main_nav form span").click(function () {
-  $(this).next().slideToggle();
-  $(this).next().focus();
-});
+
+// // Main Nav Search input
+// $("nav.navbar form span").click(function () {
+//   $(this).next().slideToggle();
+//   $(this).next().focus();
+// });
 
 // Main Nav Scroll Fixed
 $(window).scroll(function () {
   if ($(window).scrollTop() > 10) {
-    $("nav.nav .main_nav").addClass("fixed");
+    $("nav.navbar").addClass("fixed");
   } else {
-    $("nav.nav .main_nav").removeClass("fixed");
+    $("nav.navbar").removeClass("fixed");
   }
 });
 
 /********** Start Nav Services Hover **********/
 // Adjust main Nav_height
-const main_nav = document.querySelector("nav.nav .main_nav");
+const main_nav = document.querySelector("nav.navbar");
 const main_nav_height = main_nav.clientHeight;
 const services_hover = document.querySelector(
-  "nav.nav .main_nav ul.links .services .services_hover"
+  "nav.navbar ul.links .services .services_hover"
 );
 services_hover.style.top = main_nav_height + "px";
 // parent
 if (window.innerWidth >= 992) {
-  $("nav.nav .main_nav ul.links li.services").hover(
+  $("nav.navbar ul.links li.services").hover(
     function () {
       services_hover.style.height = "500px";
-      $("nav.nav .main_nav ul.links .services .services_hover").slideDown(300);
+      $("nav.navbar ul.links .services .services_hover").slideDown(300);
       setTimeout(() => {
-        $("nav.nav .main_nav ul.links .services .services_hover").addClass(
-          "active"
-        );
+        $("nav.navbar ul.links .services .services_hover").addClass("active");
       }, 301);
     },
     function () {
-      $("nav.nav .main_nav ul.links .services .services_hover").removeClass(
-        "active"
-      );
-      $("nav.nav .main_nav ul.links .services .services_hover").slideUp(300);
+      $("nav.navbar ul.links .services .services_hover").removeClass("active");
+      $("nav.navbar ul.links .services .services_hover").slideUp(300);
     }
   );
   $("span.arrow_up").hover(function () {
-    $("nav.nav .main_nav ul.links .services .services_hover").removeClass(
-      "active"
-    );
-    $("nav.nav .main_nav ul.links .services .services_hover").slideUp();
+    $("nav.navbar ul.links .services .services_hover").removeClass("active");
+    $("nav.navbar ul.links .services .services_hover").slideUp();
   });
 } else {
-  $("nav.nav .main_nav ul.links li.services").click(function () {
-    services_hover.style.height = "600px";
-    $("nav.nav .main_nav ul.links .services .services_hover").slideToggle(500);
-    setTimeout(() => {
-      $("nav.nav .main_nav ul.links .services .services_hover").toggleClass(
-        "active"
-      );
-    }, 501);
+  $("nav.navbar ul.links li.services").click(function () {
+    $(
+      "nav.navbar ul.links .services .services_drop_down.small_screen_only"
+    ).slideToggle(500);
+    $("nav.navbar ul.links .services .arrow_parent").toggleClass("active");
   });
-  $("span.arrow_up").click(function () {
-    $("nav.nav .main_nav ul.links .services .services_hover").removeClass(
-      "active"
-    );
-    $("nav.nav .main_nav ul.links .services .services_hover").slideUp();
+  $(
+    "nav.navbar ul.links .services .services_drop_down.small_screen_only"
+  ).click(function (e) {
+    e.stopPropagation();
   });
-  $("nav.nav .main_nav ul.links .services .services_hover").click(function (e) {
+  // #######################
+  $(
+    "nav.navbar ul.links .services .services_drop_down.small_screen_only li.drop_down_link"
+  ).click(function () {
+    $(this).find(".sub_drop_down").slideToggle();
+    $(this).find(".arrow").toggleClass("active");
+  });
+  $(
+    "nav.navbar ul.links .services .services_drop_down.small_screen_only li.drop_down_link .sub_drop_down"
+  ).click(function (e) {
     e.stopPropagation();
   });
 }
 
 // childs
-if (window.innerWidth >= 992) {
-  $("nav.nav .main_nav ul.links .services .services_hover ul.tabs li").hover(
-    function () {
-      $(this).addClass("active").siblings().removeClass("active");
-      services_hover.style.height =
-        $($(this).data("active")).height() + 150 + "px";
-      $($(this).data("active"))
-        .addClass("active")
-        .siblings()
-        .removeClass("active");
-    }
-  );
-} else {
-  $("nav.nav .main_nav ul.links .services .services_hover ul.tabs li").click(
-    function () {
-      $(this).addClass("active").siblings().removeClass("active");
-      services_hover.style.height =
-        $($(this).data("active")).height() + 220 + "px";
-      $($(this).data("active"))
-        .addClass("active")
-        .siblings()
-        .removeClass("active");
-    }
-  );
-}
+$("nav.navbar ul.links .services .services_hover ul.tabs li").hover(
+  function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    services_hover.style.height =
+      $($(this).data("active")).height() + 150 + "px";
+    $($(this).data("active"))
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+  }
+);
 /********** End Nav Services Hover **********/
