3c7f92ed6da2125a7071934edaad36bfefae4f07
Omar <omarelzhery236@gmail.com>
2021-02-24 14:39:18 +0200

    d

--- js/pages/tranining.js
+++ js/pages/tranining.js
@@ -1,14 +1,3 @@
-// Filters
-$(".services_filter .filter_section .upper").click(function () {
-  $(this).parent().find("form").slideToggle();
-});
-
-// Faq
-$(".services_filter .faq .accordions .accordion h2").click(function () {
-  $(this).next(".accordion_content").slideToggle();
-  $(this).find("span:last-child").toggleClass("active");
-});
-
 // Clients Slider
 $(".clients_slider").slick({
   centerMode: false,
